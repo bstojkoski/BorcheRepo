a266df9bb689cd5582115ff07a9a01590e13e8fa
borche95 <borche.stojkoski@allocatesoftware.com>
2020-03-19 11:12:20 +0100

    Modifications.

--- startApp.ps1
+++ startApp.ps1
@@ -1,4 +1,4 @@
 ﻿cd 'C:\Program Files (x86)\Go Agent\pipelines\DeployPipeline\BuildArtifacts\csEshopWeb\src\Web'
 dotnet run
-ping localhost:5106
-start localhost:5106
+ping http://localhost:5106
+start http://localhost:5106
