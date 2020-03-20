5aa8c6bc8ed224c4b31303d1b839380836fdd38a
borche95 <borche.stojkoski@allocatesoftware.com>
2020-03-20 16:03:00 +0100

    Final eShop config for master

--- csEshopWeb/CreateWebApplication.ps1
+++
@@ -1,9 +0,0 @@
-﻿param(
-    [string]$ApplicationPath,
-    [string]$SourcePath,
-    [string]$WebAppPool,
-    [string]$ApplicationName,
-    [string]$WebSiteName
-)
-
-New-WebApplication -Name $ApplicationName -Site $WebSiteName -PhysicalPath $ApplicationPath -ApplicationPool $WebAppPool -Force
