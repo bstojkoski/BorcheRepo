65f6859b4e00a8982c9105aeb30868debcf206fa
borche95 <borche.stojkoski@allocatesoftware.com>
2020-03-20 15:29:58 +0100

    Configured eShop on v20.3.1

---
+++ csEshopWeb/CreateWebApplication.ps1
@@ -0,0 +1,9 @@
+﻿param(
+    [string]$ApplicationPath,
+    [string]$SourcePath,
+    [string]$WebAppPool,
+    [string]$ApplicationName,
+    [string]$WebSiteName
+)
+
+New-WebApplication -Name $ApplicationName -Site $WebSiteName -PhysicalPath $ApplicationPath -ApplicationPool $WebAppPool -Force
