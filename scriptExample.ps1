e6c623f96d03176b76045d4de931e38bb60e1a99
bstojkoski <57131236+bstojkoski@users.noreply.github.com>
2023-06-13 09:38:30 +0200

    Create scriptExample.ps1

---
+++ scriptExample.ps1
@@ -0,0 +1,10 @@
+﻿param(
+    [string]$ApplicationPath,
+    [string]$SourcePath,
+    [string]$WebAppPool,
+    [string]$ApplicationName,
+    [string]$WebSiteName
+)
+
+Import-Module WebAdministration
+New-WebApplication "-Name eshop -Site 'Default Web Site' -PhysicalPath 'C:\\Program Files (x86)\\Go Agent\\pipelines\\e-Shop-build\\csEshopWeb' -ApplicationPool 'DefaultAppPool' -Force"
